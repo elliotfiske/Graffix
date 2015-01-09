f3f836fada2601869ea50de068d969d09ae5929e
elliotfiske <elliotfiske@gmail.com>
2015-01-08 21:56:48 -0800

    Added in base classes for Triangles and stuff

---
+++ FirstThing/Triangle.cpp
@@ -0,0 +1,9 @@
+//
+//  Triangle.cpp
+//  FirstThing
+//
+//  Created by Elliot Fiske on 1/8/15.
+//  Copyright (c) 2015 Elliot Fiske. All rights reserved.
+//
+
+#include "Triangle.h"
---
+++ FirstThing/Triangle.h
@@ -0,0 +1,14 @@
+//
+//  Triangle.h
+//  FirstThing
+//
+//  Created by Elliot Fiske on 1/8/15.
+//  Copyright (c) 2015 Elliot Fiske. All rights reserved.
+//
+
+#ifndef __FirstThing__Triangle__
+#define __FirstThing__Triangle__
+
+#include <stdio.h>
+
+#endif /* defined(__FirstThing__Triangle__) */
